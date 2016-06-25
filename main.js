4f78028a02fd4b66067c023e49deacd94a66c0ea
Alexander0106 <Alexander14159@gmail.com>
2016-06-25 17:23:05 +0800

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var bglmg=document.createElement("img");
+bglmg.src="images/map.png";
+var canvas=document.getElementByld("game-canvas");
+var ctx=canvas.getContext("2d");
+function draw(){
+  ctx.drawImage(bglmg,0,0);
+}
+setTimeout(draw,1000);
