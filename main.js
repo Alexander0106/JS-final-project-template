1d30742b57d4228821e5bbb220785e07ce3ee0b2
Alexander0106 <Alexander14159@gmail.com>
2016-07-02 16:37:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
-var ctImg=document.createElement("img");
-ctImg.src="images/";
-var canvas=document.getElementByld("game-canvas");
-var ctx=canvas.getContext("2d");
 var hero={
   x:0,
   y:0
+};
+var heroImg=document.createElement("img");
+heroImg.src="images/rukia.gif";
 }
+var ctImg=document.createElement("img");
+ctImg.src="images/tower-btn.png";
+var canvas=document.getElementByld("game");
+var ctx=canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,hero.x,hero.y);
+  ctx.drawImage(heroImg,hero.x,hero.y);
+  ctx.drawImage(ctImg,590,430,50,50);
 }
 setInterval(draw,40);
