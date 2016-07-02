3e0b17cfc1a538729b0cdeeaf03deed3fd752226
Alexander0106 <Alexander14159@gmail.com>
2016-07-02 16:28:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var bglmg=document.createElement("img");
 bglmg.src="images/map.png";
+var ctImg=document.createElement("img");
+ctImg.src="images/";
 var canvas=document.getElementByld("game-canvas");
 var ctx=canvas.getContext("2d");
 var hero={
