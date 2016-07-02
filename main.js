6adbe20e93ff06a11c4a181bf77a2dd0d1da48dd
Alexander0106 <Alexander14159@gmail.com>
2016-07-02 17:19:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $("#game").mousemove(function(event){
   cursor.x=event.offsetX;
   cursor.y=event.offsetY;
 });
-var canvas=document.getElementByld("game");
+var canvas=document.getElementByid("game");
 var ctx=canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bglmg,hero.x,hero.y);
